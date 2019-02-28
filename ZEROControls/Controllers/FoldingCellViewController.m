9ce52d0e26e77ab8c5a7beabdfc29ce888f8d24c
smile-git <zhao_wx163@163.com>
2019-02-28 15:48:43 +0800

    配置下OC调用Swift； 添加新VC，FoldingCellViewController；

---
+++ ZEROControls/Controllers/FoldingCellViewController.m
@@ -0,0 +1,33 @@
+//
+//  FoldingCellViewController.m
+//  ZEROControls
+//
+//  Created by ZWX on 2019/2/28.
+//  Copyright © 2019 ZWX. All rights reserved.
+//
+
+#import "FoldingCellViewController.h"
+#import "ZEROControls-Swift.h"
+
+@interface FoldingCellViewController ()
+
+@end
+
+@implementation FoldingCellViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
